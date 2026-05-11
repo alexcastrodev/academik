fdf33b3893144db3da82cb6e3d9998f82610cd5f
alexcastrodev <alexoliveira7x@gmail.com>
2026-05-11 18:07:17 +0100

    fix: update initialization script to source environment variables

--- initdb/01-create-role.sh
+++ initdb/01-create-role.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -e
+
+ENV_PATH="/mnt/ssd/@docker/academik/.env"
+set -a
+source "$ENV_PATH"
+set +a
+
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   ALTER DATABASE "${POSTGRES_DB}" OWNER TO "${POSTGRES_USER}";
